115103f0e16b125b6d3a47e336159af837de2970
SirGiulianaDave <93579423+SirGiulianaDave@users.noreply.github.com>
2022-11-27 21:42:32 +0100

    Initial commit

---
+++ app/components/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <Page>
+    <ActionBar>
+      <Label text="UnwantedTherapy" class="font-bold text-lg" />
+    </ActionBar>
+
+    <GridLayout>
+      <Label
+        class="text-xl align-middle text-center text-black-500"
+        :text="message"
+        @tap="logMessage"
+      />
+    </GridLayout>
+  </Page>
+</template>
+
+<script lang="ts">
+import Vue from 'nativescript-vue';
+
+export default Vue.extend({
+  computed: {
+    message() {
+      return 'Click here to create your profile!';
+    },
+  },
+
+  methods: {
+    logMessage() {
+      console.log('Get fucked Linus');
+    },
+  },
+});
+</script>
+
+<style>
+/* .info {
+    font-size: 20;
+  } */
+</style>
